f826d75f2292cd8a4828fe499fb533268c774cd3
Sodiq Sanusi <ademilddiq@gmail.com>
2022-09-01 18:02:08 +0100

    Added error styling to form validation

--- components/Form/index.js
+++ components/Form/index.js
@@ -1,10 +1,24 @@
+import { useState } from 'react';
 import styles from './Form.module.css';
 
 const Form = () => {
 
+  let [url, setUrl] = useState('')
+  let [isUrlInvalid, changeValidation] = useState(false);
+
   let handleSubmit = (e) => {
     e.preventDefault();
     console.log('Link submitted')
+    let isUrlValid = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/.test(url);
+    if(isUrlValid){
+      console.log('A URL!')
+      changeValidation(false);
+      // todo: Use the API here to shorten the valid link!
+    } else{
+      console.log('Not a url')
+      changeValidation(true);
+      // todo: Put in the errors
+    }
   }
 
   return ( 
@@ -12,8 +26,11 @@ const Form = () => {
       <div>
         <input
          type="url" placeholder="Shorten a link here..."
+         value={url}
+         onChange={(e) => setUrl(e.target.value)}
+         className={styles[isUrlInvalid]}
         />
-        <p className={styles.errMessage}></p>
+        {isUrlInvalid && <p className={styles.errMessage}>Please add a valid link</p>}
       </div>
       <button>Shorten it!</button>
     </form>
