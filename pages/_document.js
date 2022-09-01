810e0508b6af2863168cb60e4e632cf7447a222b
Sodiq Sanusi <ademilddiq@gmail.com>
2022-09-01 10:56:45 +0100

    Cleanup of boilerplate, initializing codebase
    Just the normal cleanups. Created the default document Next.js file, added the fonts to be used, cleared the CSS with addition of my preferred CSS reset, just clearing unneeded code & creating the foundation for the web app.

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang='en'>
+      <Head>
+        <meta name='author' content='Sodiq Sanusi'/>
+        <meta name='description' content='A web application that allows you shorten links.'/>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap" rel="stylesheet" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
