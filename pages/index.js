5a6a8d8a88777a2b965bfe8631ff51bdb2fbdae1
Sodiq Sanusi <ademilddiq@gmail.com>
2022-09-02 18:40:32 +0100

    Bugs. Everywhere!
    I've tried making the functionalities work on mobile for the past 4 hours? I really don't understand what to do at the moment. Anyways, I've decided to move on to the styling on large screen devices, will work on the functionalities when I'm done with that.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect } from 'react'
 import BoostSection from '../components/BoostSection'
 import Footer from '../components/Footer'
 import Form from '../components/Form'
@@ -8,6 +9,7 @@ import MainSection from '../components/MainSection'
 import Navbar from '../components/Navbar'
 
 export default function Home() {
+
   return (
     <>
       <Head>
